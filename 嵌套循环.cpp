8f20fb447860ffdfb1654e6e71bcbdbe2252f49a
GSJJ <1708851071@qq.com>
2026-04-09 22:50:07 +0800

    准备进入数组

---
+++ 嵌套循环.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	char star = '*';
+//
+//
+//	for (int i = 1; i < 11; i++) 
+//	{
+//		for (int j = 1; j < 11; j++) 
+//		{
+//			cout << star;
+//
+//			if (j == 10) 
+//			{
+//				cout<< "\n" << endl;
+//			};
+//		}
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
